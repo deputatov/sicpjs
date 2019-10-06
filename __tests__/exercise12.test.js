c2995ff58103ba09091f2b53fc2cd66e12d7aa38
Deputatov Nikita <deputatovn@yandex.ru>
2019-10-06 18:46:37 +0300

    add test coverage

--- __tests__/exercise12.test.js
+++ __tests__/exercise12.test.js
@@ -1,5 +1,7 @@
 import exercise12 from '../src/exercise12';
 
 test('exercise12', () => {
-  expect(exercise12()).toEqual('(5 + 4 + (2 - (3 - (6 + 4 / 5)))) / (3 * (6 - 2) * (2 - 7))');
+  expect(exercise12()).toEqual(
+    '(5 + 4 + (2 - (3 - (6 + 4 / 5)))) / (3 * (6 - 2) * (2 - 7))',
+  );
 });
